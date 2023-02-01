e9e9f14a8059c0ca1227e897f50e9e9212b4a428
Titus Mumo <titusmumo@gmail.com>
2023-02-01 08:51:09 +0300

    adding picture slides to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,39 @@
 import React from 'react'
 import Userbar from '../components/Navbar'
+import Slides from '../components/Slides';
 
 function Home() {
   return (
     <>
     <Userbar/>
+
+    <Slides/>
+    <>
+
+    <>
+    <div>
+      <h1 className='text-center mt-2'>Our Services</h1>
+      <section className="py-4 container" style={{ background: '#fff' }}>
+        <p>We are mandated with providing cost effective and affordable quality water and sanitation services to the residents of Kisumu County.</p>
+        <div className="row justify-content-center">
+          <div className="col-11 col-md-6 col-lg-3 mx-0 mb-4">
+          <div className="card p-0overflow-hidden-100 shadow">
+          <div className="card-body" style={{ background: '#4BB49B4D' }}>
+            <h5 className="card-title">Water Supply</h5>
+            <p className="card-text">description</p>
+            {/* <p className="text-center" style={{ mouse: 'pointer', fontweight: 'bold' }}>
+              Learn More
+            </p> */}
+          </div>
+          </div>
+          </div>
+        </div>
+      </section>
+    </div>
     </>
+    </>
+    </>
+    
   )
 }
 
