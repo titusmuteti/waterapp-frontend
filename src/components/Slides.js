b3fcd54196bef429934058ba715b297e500e2a9f
Titus Mumo <titusmumo@gmail.com>
2023-03-15 19:20:47 +0300

    editing home page

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -7,7 +7,7 @@ function Slides() {
     <Carousel.Item className='h-100'>
       <img
         className="d-block w-100" 
-        src="https://thumbs.dreamstime.com/b/young-african-boy-drinking-water-community-borehole-hand-pump-young-african-boy-drinking-water-community-172697689.jpg"
+        src={"/images/boydrinkingwater.jpg"}
         height='600'
         alt="First slide"
       />
@@ -19,7 +19,7 @@ function Slides() {
     <Carousel.Item>
       <img
         className="d-block w-100"
-        src="https://media.licdn.com/dms/image/C5612AQF_Q-gZCDdpaw/article-cover_image-shrink_600_2000/0/1552917949726?e=2147483647&v=beta&t=dcwMIA72Ozt71VI4kYrnUjql6Ew6F-7jD0ncLxDTvVs"
+        src="/images/tap.jpeg"
         height='600'
         alt="Second slide"
       />
@@ -27,7 +27,7 @@ function Slides() {
     <Carousel.Item>
       <img
         className="d-block w-100"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxCXvZtUfH_9z3kHaSaWUZV17JDFujC72gaEiP11FMId5wOcmflBeyMq7ZxdIwlL4bswQ&usqp=CAU"
+        src="/images/waterdriller.jpeg"
         height='600'
         alt="Third slide"
       />
--- src/data.js
+++ src/data.js
@@ -2,17 +2,17 @@ const data = {
     servicedata: [
         {
             title: "Water Supply",
-            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjAbaLbbQ_Mu0wBHUeGb7M-Y4Y6zwAAviu8w&usqp=CAU",
+            image: "/images/pipeConnections.jpeg",
             desc: "Water supply is the provision of water by public utilities, commercial organisations, community endeavors or by individuals, usually via a system of pumps and pipes. Public water supply systems are crucial to properly functioning societies.", 
         },
         {
             title: "Sanitation Services",
-            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNrzIt3vM1G2kZUrdrOxsg1iSVBtZWhV_-96dF1528_9qC19T49CHzJ893SKIZReHlmBI&usqp=CAU",
+            image: "/images/waterFetching.jpeg",
             desc: "Basic sanitation is defined as having access to facilities for the safe disposal of human waste (feces and urine), as well as having the ability to maintain hygienic conditions, through services such as garbage collection, industrial/hazardous waste management, and wastewater treatment and disposal.", 
         },
         {
             title: "Water Treatment",
-            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4za0kj-ZFDxtKFY0JPFx4lOGaXKD7JgdvQQ&usqp=CAU",
+            image: "/images/water.jpeg",
             desc: "Water treatment is any process that improves the quality of water to make it appropriate for a specific end-use. The end use may be drinking, industrial water supply, irrigation, river flow maintenance, water recreation or many other uses, including being safely returned to the environment.", 
         },
     ]
