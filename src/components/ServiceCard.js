ab741dff4970e776e156c8a9d0062ff8505f8821
Titus Mumo <titusmumo@gmail.com>
2023-06-10 11:20:25 +0300

    add responsiveness and editing service cards

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const ServiceCard = (props) => {
   return (
-    <div className="col-11 col-md-6 col-lg-3 mx-0 mb-4 p-5">
-      <div className="card p-0overflow-hidden-100 shadow">
+    <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+      <div className="card h-100">
         <div className="card-body" style={{ background: '#4BB49B4D' }}>
           <h3 className="card-title">{props.title}</h3>
-          <img className='w-100' class="card-title" src={props.image} alt={props.title} />
+          <img className="w-100 card-img-top" src={props.image} alt={props.title} />
           <p className="card-text mt-4">{props.desc}</p>
         </div>
       </div>
