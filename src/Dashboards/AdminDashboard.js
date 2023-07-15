2ce044725842f6a8169c5cf640158bff7d11cc0e
Titus Mumo <titusmumo@gmail.com>
2023-07-15 19:51:34 +0300

    styling the table in admin dashboard

--- src/Dashboards/AdminDashboard.js
+++ src/Dashboards/AdminDashboard.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { BiTrash } from 'react-icons/bi';
+import { BiTrash, BiEdit } from 'react-icons/bi';
+import './AdminDashboard.css'
 
 function AdminDashboard() {
   const [data, setData] = useState([]);
@@ -14,7 +15,7 @@ function AdminDashboard() {
   const fetchData = async () => {
     try {
       // Make the API request
-      const response = await fetch('https://makawasco-backend.onrender.com/clients');
+      const response = await fetch('https://makawasco-backend.onrender.com/bills');
       const jsonData = await response.json();
       // Update the data state with the received data
       setData(jsonData);
@@ -64,39 +65,47 @@ function AdminDashboard() {
           <thead className="thead-dark sticky-top">
             <tr>
               <th>#</th>
-              <th style={{ width: '150px' }}>First Name</th>
-              <th style={{ width: '150px' }}>Last Name</th>
-              <th style={{ width: '250px' }}>Email</th>
-              <th style={{ width: '200px' }}>Phone Number</th>
-              <th style={{ width: '100px' }}>Date Read</th>
-              <th style={{ width: '100px' }}>Previous Reading</th>
-              <th style={{ width: '100px' }}>Current Reading</th>
-              <th style={{ width: '250px' }}>Read By</th>
-              <th style={{ width: '100px' }}>Balance</th>
-              <th style={{ width: '100px' }}>Payment Status</th>
-              <th style={{ width: '100px' }}>Date Paid</th>
-              <th style={{ width: '100px' }}>Actions</th>
+              <th style={{ width: '10%', whiteSpace: 'nowrap' }}>First Name</th>
+              <th style={{ width: '10%', whiteSpace: 'nowrap' }}>Last Name</th>
+              <th style={{ width: '15%', whiteSpace: 'nowrap' }}>Email</th>
+              <th style={{ width: '10%', whiteSpace: 'nowrap' }}>Phone Number</th>
+              <th style={{ width: '10%', whiteSpace: 'nowrap' }}>Date Read</th>
+              <th style={{ width: '5%', whiteSpace: 'nowrap' }}>Previous Reading</th>
+              <th style={{ width: '5%', whiteSpace: 'nowrap' }}>Current Reading</th>
+              <th style={{ width: '10%', whiteSpace: 'nowrap' }}>Read By</th>
+              <th style={{ width: '5%', whiteSpace: 'nowrap' }}>Balance</th>
+              <th style={{ width: '10%', whiteSpace: 'nowrap' }}>Payment Status</th>
+              <th style={{ width: '10%', whiteSpace: 'nowrap' }}>Date Paid</th>
+              <th style={{ width: '10%', whiteSpace: 'nowrap' }}>Actions</th>
+
             </tr>
           </thead>
           <tbody>
             {currentRecords.map((item, index) => (
               <tr key={index}>
                 <td>{index + 1}</td>
-                <td>{item.first_name}</td>
-                <td>{item.last_name}</td>
-                <td>{item.email}</td>
-                <td>{item.phone_number}</td>
-                <td>{item.date_read}</td>
-                <td>{item.previous_reading}</td>
-                <td>{item.current_reading}</td>
-                <td>{item.balance}</td>
-                <td>{item.payment_status}</td>
+                <td style={{ whiteSpace: 'nowrap' }}>{item.client.first_name}</td>
+                <td style={{ whiteSpace: 'nowrap' }}>{item.client.last_name}</td>
+                <td style={{ whiteSpace: 'nowrap' }}>{item.client.email}</td>
+                <td style={{ whiteSpace: 'nowrap' }}>{item.client.phone_number}</td>
+                <td style={{ whiteSpace: 'nowrap' }}>{item.date_read}</td>
+                <td style={{ whiteSpace: 'nowrap' }}>{item.previous_reading}</td>
+                <td style={{ whiteSpace: 'nowrap' }}>{item.current_reading}</td>
+                <td style={{ whiteSpace: 'nowrap' }}>{item.read_by}</td>
+                <td style={{ whiteSpace: 'nowrap' }}>{item.balance}</td>
+                <td style={{ whiteSpace: 'nowrap' }}>{item.payment_status}</td>
+                <td style={{ whiteSpace: 'nowrap' }}>{item.date_paid}</td>
                 <td>
-                  {item.date_paid}
-                  <button onClick={() => handleDelete(index)} className="btn btn-danger ml-2">
-                    <BiTrash />
-                    <span className="ml-1">Delete</span>
-                  </button>
+                  <div className="action-icons">
+                    <button onClick={() => handleDelete(index)} className="btn btn-danger">
+                      <BiTrash />
+                      <span>Delete</span>
+                    </button>
+                    <button className="btn btn-primary">
+                      <BiEdit />
+                      <span>Edit</span>
+                    </button>
+                  </div>
                 </td>
               </tr>
             ))}
@@ -104,29 +113,30 @@ function AdminDashboard() {
         </table>
       </div>
       <div className="pagination">
-        {currentPage > 1 && (
-          <button onClick={handlePreviousPage} className="btn btn-primary">
-            Previous
-          </button>
-        )}
-        {Array.from({ length: totalPages }, (_, index) => (
-          <button
-            key={index}
-            onClick={() => handlePageChange(index + 1)}
-            className={`btn ${currentPage === index + 1 ? 'btn-primary' : 'btn-secondary'}`}
-          >
-            {index + 1}
-          </button>
-        ))}
-        {currentRecords.length === recordsPerPage && (
-          <button onClick={handleNextPage} className="btn btn-primary">
-            Next
-          </button>
-        )}
+        <div className="pagination-buttons">
+          {currentPage > 1 && (
+            <button onClick={handlePreviousPage} className="btn btn-primary">
+              Previous
+            </button>
+          )}
+          {Array.from({ length: totalPages }, (_, index) => (
+            <button
+              key={index}
+              onClick={() => handlePageChange(index + 1)}
+              className={`btn ${currentPage === index + 1 ? 'btn-primary' : 'btn-secondary'}`}
+            >
+              {index + 1}
+            </button>
+          ))}
+          {currentRecords.length === recordsPerPage && (
+            <button onClick={handleNextPage} className="btn btn-primary">
+              Next
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
 }
 
 export default AdminDashboard;
-  
