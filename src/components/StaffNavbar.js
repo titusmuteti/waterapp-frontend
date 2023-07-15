e1c6c3e59eb6aba1cd80476401dfa8f39807f071
Titus Mumo <titusmumo@gmail.com>
2023-07-15 20:29:58 +0300

    styling the table in staffnavbar

--- src/components/StaffNavbar.js
+++ src/components/StaffNavbar.js
@@ -8,7 +8,7 @@ const MiniNavbar = () => {
         <a href="/admin">Admin login</a>
         <a href="/staff">Staff login</a>
         <a href="tel:0700000000">Contact: 0700 000 000</a>
-        <a href="email:we@makawaco.co.ke">email:we@makawaco.co.ke</a>
+        {/* <a href="email:we@makawaco.co.ke">email:we@makawaco.co.ke</a> */}
       </div>
     </nav>
   );
